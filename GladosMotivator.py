d04e4daf0aa79bf749b3ca3f82b9f218222553b8
Mycoshi <67665537+Mycoshi@users.noreply.github.com>
2021-12-11 12:03:30 -0700

    MASSIVE CHANGES -working stop button -working signal lights - basic orientation of widgets

--- GladosMotivator.py
+++ GladosMotivator.py
@@ -27,6 +27,7 @@ class Core(tk.Tk):
 
 
 # UI
+#BUILD TIMER STANDING 0:00 LABEL
         self.title('GladOsMotivator')
         self.geometry('800x530')
 
@@ -37,24 +38,31 @@ class Core(tk.Tk):
         self.canvas.grid(column=1, row=0)
 
         self.start_button =  tk.Button(text="Start", command=self.start_timer)
-        self.start_button.place(x=25, y=380)
+        self.start_button.place(x=100, y=450)
 
-        self.reset_button =  tk.Button(text="Reset")
-        self.reset_button.place(x=100, y=380)
+        self.stop_button =  tk.Button(text="Stop", command=self.stop_timer)
+        self.stop_button.place(x=175, y=450)
 
         # trying to flip switches from red to green to indicate mode
         self.worktimentry = tk.Entry(bg='black',fg='white')
         self.worktimentry.insert(0,'1')
-        self.worktimentry.place(x=200,y=380, width = 20)
+        self.worktimentry.place(x=120,y=380, width = 20)
         self.worktimelabel = tk.Label(text='work',fg='#000000')
-        self.worktimelabel.place(x=225,y=380)
+        self.worktimelabel.place(x=120,y=280)
+
+
+        #making signal light
+        self.signallabel = tk.Label(text='I',bg='green', height=5, width=10, justify='center', relief='sunken')
+        self.signallabel.place(x=100,y=300)
+        self.signallabel2 = tk.Label(text='O',bg='red', height=5, width=10, justify='center', relief='raised')
+        self.signallabel2.place(x=175,y=300)
 
 
         self.breaktimentry = tk.Entry(bg='black',fg='white')
         self.breaktimentry.insert(0,'1')
-        self.breaktimentry.place(x=200,y=400, width = 20)
+        self.breaktimentry.place(x=205,y=380, width = 20)
         self.breaktimelabel = tk.Label(text='break')
-        self.breaktimelabel.place(x=225,y=400)
+        self.breaktimelabel.place(x=200,y=280)
 
 
         self.worktime = int(self.worktimentry.get())
@@ -64,12 +72,12 @@ class Core(tk.Tk):
         #button color switch working 
     def rLightGreenLight(self):
         if self.work == True:
-            self.worktimelabel.config (text='working',bg='#47d60f')
-            self.breaktimelabel.config (text='no cake'bg='#d60f0f')
+            self.signallabel.config (relief='sunken')
+            self.signallabel2.config (relief='raised')
             print("greenlight")
         else:
-            self.worktimelabel.config (text='not working',bg='#d60f0f')
-            self.breaktimelabel.config (text='cake' bg='#47d60f')
+            self.signallabel.config (relief='raised')
+            self.signallabel2.config (relief='sunken')
             print("redlight")
 
 
@@ -90,32 +98,35 @@ class Core(tk.Tk):
 
 #TODO CHECK IF TIMER BUG STILL EXISTS 
     def count_down(self, count):
-        self.count_min = math.floor(count / 60)
-        self.count_sec = count % 60
-        Timer = tk.Label(text=f" {self.count_min} : {self.count_sec} ", fg="white", bg='black')
-        Timer.place(x=150,y=380)
-        if count > 0:
-            print(count)
-            self.after(1000, self.count_down, count - 1)
-        elif count == 0:
-            print('switch!')
-            global work 
-            if self.work == True:
+        if self.running == True:
+            self.count_min = math.floor(count / 60)
+            self.count_sec = count % 60
+            Timer = tk.Label(text=f" {self.count_min} : {self.count_sec} ", fg="white", bg='#4b5d69')
+            Timer.place(x=150,y=50)
+            if count > 0:
+                print(count)
+                self.after(1000, self.count_down, count - 1)
+            elif count == 0:
+                print('switch!')
+                global work 
                 self.play_Line()
-            else:
-                self.play_Line()
-            self.work = ~ self.work
-            self.rLightGreenLight()
-                
-            self.start_timer()
+                self.work = ~ self.work
+                self.rLightGreenLight()
+                self.start_timer()
+        else:
+            return
 
     def start_timer(self):
+        self.running = True
         if self.work == True:
             worktime = int(self.worktimentry.get())
             self.count_down(worktime * 5)
         else:
             breaktime = int(self.breaktimentry.get())
             self.count_down(breaktime*5)
+    def stop_timer(self):
+        self.running = False
+        
 
 
 
@@ -127,12 +138,7 @@ if __name__ == "__main__":
     app.mainloop()
 
         
-        #TODO radio buttons for different break timers
-        #radio buttons were switched out for customizable entry extra points if we create a lock but seems just aesthetic
-        #TODO Noise packs and implentation
-        #noise packs exist and are implented with playsound 1.2.2 next to create either a dict or a list of available packs and randomize
-        #TODO resize buttons and windows
-        # need bolds weights for labels and possible resizing of entry widgets, coloring can be done with hexidecimals 
-        # I need to finalize backround image in an editor before placement 
-        #im bad at graphic design some i waiting till the backend is up and functional before placement probably place this last
-        #TODO add appenable todo list with crossout or checkbox 
+     #noise packs exist and are implented with playsound 1.2.2 next to create either a dict or a list of available packs and randomize
+    # need bolds weights for labels and possible resizing of entry widgets, coloring can be done with hexidecimals 
+    #TODO add appenable todo list with crossout or checkbox 
+    # Timer functional but start button does not start at previous position 
