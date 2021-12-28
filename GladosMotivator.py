cec0ea3b31a12ef4f374ac466b397e851bb9e026
Mycoshi <67665537+Mycoshi@users.noreply.github.com>
2021-12-28 12:02:48 -0700

    updated comments
    started tasklist ran through comments
    -CAREFUL ASYNC IS BROKEN

--- GladosMotivator.py
+++ GladosMotivator.py
@@ -1,21 +1,19 @@
 # Break study machine
 
 import tkinter as tk 
-import time
+from tkinter.ttk import Label, Style
+from tkinter import Listbox, Tk
 import math
 import random
 import glob
 #playsound requires version 1.2.2 new version is broken?
 from playsound import playsound
 import os
-from tkinter.ttk import Label, Style
-from tkinter import Tk
-
-# Play with this whe ready for async voicelines
-#import asyncio
+import asyncio
+import tkinter.messagebox
 
 
-
+# 12/28/21 CANT USE AYSYNC FUNTION DURING SYNC EVENT... which means every function has to be labeled aysnc and put in the same 'task set'
 
 class Core(tk.Tk):
     def __init__(self):
@@ -33,7 +31,9 @@ class Core(tk.Tk):
 
 # UI
 #BUILD TIMER STANDING 0:00 LABEL
-#font fixd with stupid easy font= must explore aperture fonts,
+
+
+#CORE
         self.title('GladOsMotivator')
         self.geometry('800x530')
 
@@ -45,39 +45,52 @@ class Core(tk.Tk):
         self.canvas.create_image(470, 270, image=self.background)
         self.canvas.grid(column=1, row=0)
 
+        #UI BUTTONS
+
         self.start_button =  tk.Button(text="Start", command=self.start_timer)
         self.start_button.place(x=100, y=450)
 
         self.stop_button =  tk.Button(text="Stop", command=self.stop_timer)
         self.stop_button.place(x=175, y=450)
 
-        # trying to flip switches from red to green to indicate mode
-        self.worktimentry = tk.Entry(bg='black',fg='white')
-        self.worktimentry.insert(0,'1')
-        self.worktimentry.place(x=120,y=380, width = 20)
-        self.worktimelabel = tk.Label(text='work',fg='#000000', font=('Helvatical bold',20))
-        self.worktimelabel.place(x=120,y=280)
-
-
-        #making signal light
+        #SIGNAL LIGHT
         self.signallabel = tk.Label(text='I',bg='green', height=5, width=10, justify='center', relief='sunken')
         self.signallabel.place(x=100,y=300)
         self.signallabel2 = tk.Label(text='O',bg='red', height=5, width=10, justify='center', relief='raised')
-        self.signallabel2.place(x=175,y=300)
+        self.signallabel2.place(x=177,y=300)
 
+        #TIME INSERTION
+        self.worktimentry = tk.Entry(bg='black',fg='white')
+        self.worktimentry.insert(0,'1')
+        self.worktimentry.place(x=120,y=380, width = 20)
+        self.worktimelabel = tk.Label(text='Work',fg='#FFFFFF',bg='#4b5d69', font=('Share Tech Mono',20))
+        self.worktimelabel.place(x=110,y=260)
 
         self.breaktimentry = tk.Entry(bg='black',fg='white')
         self.breaktimentry.insert(0,'1')
         self.breaktimentry.place(x=205,y=380, width = 20)
-        self.breaktimelabel = tk.Label(text='break')
-        self.breaktimelabel.place(x=200,y=280)
+        self.breaktimelabel = tk.Label(text='Break', bg='#4b5d69', fg='#FFFFFF',font=('Share Tech Mono',20))
+        self.breaktimelabel.place(x=180,y=260)
 
 
         self.worktime = int(self.worktimentry.get())
         self.breaktime = int(self.breaktimentry.get())
+        
+
+        #tasklist
+        self.listbox_task = tkinter.Listbox(self, height=3, width=50)
+        self.listbox_task.place(x=50,y=50)
+
+        self.enter_task =  tkinter.Entry(self, width=50)
+        self.enter_task.place(x=50,y=50)
+
+        self.enter_taskclick = tkinter.Button(self, text= '+', width=12, command=self.add_task)
+        self.enter_taskclick.place (x=75,y=75)
 
 
-        #button color switch working 
+
+
+        #possible artifact from config days
     def rLightGreenLight(self):
         if self.work == True:
             self.signallabel.config (relief='sunken')
@@ -94,7 +107,7 @@ class Core(tk.Tk):
 # 1- here a combination of os.path.dirname(os.path.abspath(__file__)) & Glob work to retrieve the place we are in and then cycle down into the folders we have knowledge of
 
 #fixed praise glob
-    def play_Line(self):
+    async def play_Line(self):
         path = os.path.dirname(os.path.abspath(__file__))
         Break_glines = glob.glob(path + "/sounds/Break_time/*")
         Work_glines = glob.glob(path + "/sounds/Work_time/*")
@@ -102,6 +115,11 @@ class Core(tk.Tk):
             playsound(Break_glines[random.randint(0,23)])
         else:
             playsound(Work_glines[random.randint(0,23)])
+# playsound only works with version 1.2.2
+    async def asyncline(self):
+
+        task = asyncio.create_task(self.play_Line)
+        await task
 
 
 #TODO CHECK IF TIMER BUG STILL EXISTS 
@@ -109,15 +127,17 @@ class Core(tk.Tk):
         if self.running == True:
             self.count_min = math.floor(count / 60)
             self.count_sec = count % 60
-            Timer = tk.Label(text=f" {self.count_min} : {self.count_sec} ", fg="white", bg='#4b5d69', font=('Helvatical bold',20))
-            Timer.place(x=150,y=50)
+            Timer = tk.Label(text=f" {self.count_min} : {self.count_sec} ", fg="white", bg='#4b5d69', font=('Share Tech Mono',20))
+            Timer.place(x=120,y=50)
             if count > 0:
                 print(count)
                 self.after(1000, self.count_down, count - 1)
             elif count == 0:
                 print('switch!')
                 global work 
-                self.play_Line()
+                #async functons are hard i guess i have to make everything concurrent
+                asyncio.ensure_future(self.play_Line())
+                asyncio.run(self.play_Line())
                 self.work = ~ self.work
                 self.rLightGreenLight()
                 self.start_timer()
@@ -136,6 +156,11 @@ class Core(tk.Tk):
         self.running = False
         
 
+    def add_task(self):
+        self.task = self.enter_task.get()
+        self.listbox_task.insert(tkinter.END,self.task)
+
+
 
 
 
@@ -145,8 +170,8 @@ if __name__ == "__main__":
     #app withdraw removes second window which was root?
     app.mainloop()
 
-        
-     #noise packs exist and are implented with playsound 1.2.2 next to create either a dict or a list of available packs and randomize
+
+     #TODO appendable list added needs formatting and decision on list insertion order. Also expanable listbox and messagebox functions?
     # need bolds weights for labels and possible resizing of entry widgets, coloring can be done with hexidecimals 
     #TODO add appenable todo list with crossout or checkbox 
     # Timer functional but start button does not start at previous position 
